1867b857f70af23093ca6d198544fa599da6c7cb
oederson <eder.hu@live.com.pt>
2023-10-11 09:43:06 -0300

    Curso 06 terminado

---
+++ 06-CSharp-criando-e-distribuindo-bibliotecas-em-NET/bytebank_ATENDIMENTO/Program.cs
@@ -0,0 +1,14 @@
+﻿using bytebank_ATENDIMENTO.bytebank.Atendimento;
+using bytebank_GeradorChavePix;
+
+Console.WriteLine("Boas Vindas ao ByteBank, Atendimento.");
+new ByteBankAtendimento().AtendimentoCliente();
+
+//Console.WriteLine(GeradorPix.GetChavePix());
+
+//var listaDeChaves = GeradorPix.GetChavesPix(10);
+
+//foreach (var chave in listaDeChaves)
+//{
+//    Console.WriteLine(chave);
+//}
